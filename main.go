b099297262d790d6b03b073951f42953bc52f02f
Max <maximilian.en.mueller@swisscows.email>
2025-05-05 20:08:11 +0200

    Added first weathericon and updated layout of ui

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"mirrorApp/ui"
 
 	"fyne.io/fyne/v2/app"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	a := app.New()
 	a.Settings().SetTheme(&(ui.CustomTheme{}))
 	w := a.NewWindow("Mirror App")
--- ui/mainWindow.go
+++ ui/mainWindow.go
@@ -1,8 +1,13 @@
 package ui
 
 import (
+	"image/color"
+	"time"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -12,19 +17,47 @@ func Create(w fyne.Window) {
 	// window should always be full screen!
 	w.SetFullScreen(true)
 
-	// create a greeting label
-	var heading *widget.Label = widget.NewLabel("Good day, " + user + "!")
-	var centerBox *fyne.Container = container.NewCenter()
-	var verticalBox *fyne.Container = container.NewVBox()
-	verticalBox.Add(heading)
-	verticalBox.Add(widget.NewButton("Show Mirror!", func() {
-		heading.SetText("ABC")
-	}))
-	verticalBox.Add(createWeatherLayout())
-	centerBox.Add(verticalBox)
-	w.SetContent(centerBox)
+	w.SetContent(container.New(layout.NewVBoxLayout(),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), createGreeting(), layout.NewSpacer()),
+		layout.NewSpacer(),
+		createWeatherLayout(),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), createCurrentDayLayout(), layout.NewSpacer()),
+		layout.NewSpacer(),
+		createTodoList(),
+		layout.NewSpacer(),
+	))
+}
+
+func createGreeting() *canvas.Text {
+	return canvas.NewText("Good day, "+user+"!", color.White)
 }
 
 func createWeatherLayout() *fyne.Container {
-	return container.NewGridWithColumns(4, widget.NewLabel("Temperature"), widget.NewLabel("Wind"), widget.NewLabel("Weather"), widget.NewLabel("Time"))
+	var weatherImage *canvas.Image = canvas.NewImageFromResource(fyne.NewStaticResource("sunny.png", ResourceSunnyPng.StaticContent))
+	weatherImage.FillMode = canvas.ImageFillOriginal
+	return container.New(layout.NewHBoxLayout(), layout.NewSpacer(),
+		weatherImage,
+		layout.NewSpacer(),
+		container.NewGridWithRows(2, canvas.NewText("22 °C", color.White), widget.NewLabel("5 °C")),
+		layout.NewSpacer(),
+	)
+}
+
+func createCurrentDayLayout() *canvas.Text {
+	return canvas.NewText(time.Now().Format(time.RFC1123), color.White)
+}
+
+func createTodoList() *fyne.Container {
+	return container.New(layout.NewVBoxLayout(),
+		canvas.NewLine(color.White),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), canvas.NewText("TODO", color.White), layout.NewSpacer()),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), widget.NewLabel("Wash dishes"), layout.NewSpacer()),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), widget.NewLabel("take umbrella"), layout.NewSpacer()),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), widget.NewLabel("Go for a walk!"), layout.NewSpacer()),
+		layout.NewSpacer(),
+	)
 }
